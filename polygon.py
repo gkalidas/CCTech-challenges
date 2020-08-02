08b599a766d90a864764ef84dd0a33b747cc37ec
glondhe <ganesh.londhe@crenexa.com>
2020-08-02 14:57:18 +0530

    intersecting function added

--- polygon.py
+++ polygon.py
@@ -35,3 +35,37 @@ def on_segment(p, q, r):
                 return True
     else:
         return False
+
+
+def do_intersect(p1, q1, p2, q2):
+    """
+    Function to check if given segments (p1,q1) and (p2,q2)
+    intersect with each other or not
+    Returns
+    True if segments intersect else False
+    """
+
+    o1 = orientation(p1, q1, p2)
+    o2 = orientation(p1, q1, q2)
+    o3 = orientation(p2, q2, p1)
+    o4 = orientation(p2, q2, q1)
+
+    # general case
+    if o1 != o2 and o3 != o4:
+        return True
+
+    # special cases
+    # check if 
+    if (o1 == 0 and on_segment(p1, p2, q1)):
+        return True
+
+    if (o2 == 0 and on_segment(p1, q2, q1)):
+        return True
+
+    if (o3 == 0 and on_segment(p2, p1, q2)):
+        return True
+
+    if (o4 == 0 and on_segment((p2, q2, q2))):
+        return True
+    
+    return False
